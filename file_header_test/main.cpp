7837635fd734950b345c0fc5106f6132a869e294
Williams, Adam <adamthewilliams@gmail.com>
2019-07-11 23:04:18 -0500

    cleaning up code

--- file_header_test/main.cpp
+++ file_header_test/main.cpp
@@ -14,13 +14,11 @@ int main()
 
 void get_file_signatures_from_file()
 {
-  ifstream    header_file;
+  ifstream    header_file{ "file_headers.txt" };
   string      current_line;
+  smatch      regex_matches{};  
 
-  smatch regex_matches{};
-  header_file.open("file_headers.txt");
-
-  while (!header_file.eof()) {
+  while (header_file.good()) {
 
     getline(header_file, current_line);
     regex_search(current_line, regex_matches, regex_pattern);
@@ -39,16 +37,12 @@ void check_for_matches(smatch& regex_matches) {
 
   if (regex_matches[EXTENSION].matched) {
     patterns.push_back(regex_matches[EXTENSION].str());
-
     if (regex_matches[HEADER].matched) {
       patterns.back().import_header_byte_array(regex_matches[HEADER].str());
-
-      if (regex_matches[FOOTER].matched) {
+      if (regex_matches[FOOTER].matched) 
         patterns.back().import_footer_byte_array(regex_matches[FOOTER].str());
-      }
-      if (regex_matches[MAX_SIZE].matched) {
+      if (regex_matches[MAX_SIZE].matched) 
         patterns.back().max_carve_size = stol(regex_matches[MAX_SIZE].str());
-      }
       else {
         cerr << "Error with max size formatting. Exiting..." << endl;
         exit(1);
@@ -66,8 +60,9 @@ void check_for_matches(smatch& regex_matches) {
 
 void test_input_for_matches(const char* input_file)
 {
-  char        current_byte;
-  ifstream    testFile{ input_file, ios::ate }; // open to EOF to get size
+  char            current_byte;
+  ifstream        testFile{ input_file, ios::ate }; // open to EOF to get size
+  PatternMatcher  PM{ patterns };
   
   auto file_size = testFile.tellg();
   
@@ -78,23 +73,17 @@ void test_input_for_matches(const char* input_file)
   // copy file into array
   testFile.read((char*)test_array, file_size);
 
-  PatternMatcher PM{ patterns };
   PM.find_matches(test_array, file_size);
 
-  for (int i = 0; i < PM.positions.size(); i++)
-  {
+  for (int i = 0; i < PM.positions.size(); i++) {
     printf("\n%s: ", patterns[i].file_extension.c_str());
-
-    for (auto header : patterns[i].header_byte_array) {
-      printf("%-2x ", (int)header);
-    }
+    
+    for (auto header : patterns[i].header_byte_array)
+      printf("%-02x ", static_cast<int>(header));    
     cout << endl;
-
-    for (auto itr =  PM.positions[i].begin(); 
-              itr != PM.positions[i].end(); 
-              ++itr) {
-      printf("\t-%l\n", (int)* itr - (patterns[i].header_byte_array.size() - 1));
-    }
+    
+    for (auto itr =  PM.positions[i].begin(); itr != PM.positions[i].end(); ++itr)
+      printf("\t-%d\n", static_cast<int>(*itr) - (patterns[i].header_byte_array.size() - 1));        
     cout << endl;
   }
 }
